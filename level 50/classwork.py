7bb1bad3a550ab81ee7be44f40235ea907c39a52
aleksbaleks10 <aleks.makharadze@gmail.com>
2025-03-26 21:20:02 +0400

    second commit

---
+++ level 50/classwork.py
@@ -0,0 +1,17 @@
+laptop = {
+    "name": "Dell XPS 13",
+    "color": "Silver",
+    "price": 1200,
+    "processor": "Intel i7",
+    "ram": "16GB",
+    "storage": "512GB SSD",
+    "screen_size": "13.3 inches"
+}
+
+keys = list(laptop.keys())
+values = list(laptop.values())
+items = list(laptop.items())
+
+print("Keys:", keys)
+print("Values:", values)
+print("Items:", items)
